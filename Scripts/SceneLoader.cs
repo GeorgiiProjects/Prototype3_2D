c633799d3a3078892c40ee90702e9de2551955b2
GeorgiiProjects <89801261+GeorgiiProjects@users.noreply.github.com>
2021-08-30 21:43:42 +0300

    Prototype3_2D
    Prototype3_2D

---
+++ Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Создаем метод для загрузки следующей сцены.
+    private void LoadNextScene()
+    {
+        // Получаем доступ к сцене которая активна т.е. на которой находится Player.
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // Переходим с активной сцены на следующую.
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // создаем метод для загрузки начального экрана
+    private void LoadStartScene()
+    {
+        // Загружаем начальный экран, или пишем 0 за место "Start Menu"
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    // создаем метод для выхода из игры
+    private void QuitGame()
+    {
+        // выходим из игры.
+        Application.Quit();
+    }
+}
